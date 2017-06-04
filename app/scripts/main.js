0f0581113d21256a4f81b074d6191ddb72cab2a1
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2017-06-04 12:51:12 +0100

    Showing/hiding search field.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,10 +2,18 @@
     'use strict';
 
     $(function () {
-        $('#menu-button').click(function () {
+        // Menu button
+        $('#menu-button').on('click tap', function () {
             $(this).toggleClass('is-active');
             $('.content-wrapper').toggleClass('menu-open');
         });
 
+        // Search field
+        var search_field =$('#search-field'),
+            search_button = $('#search-button');
+
+        search_button.on('click tap', function () {
+            search_field.toggleClass('is-active');
+        });
     });
 })();
